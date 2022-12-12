ccb124358f63e6ab71463e83817e1102ebb4e277
Manuel <manuneva@ucm.es>
2022-12-12 10:41:48 +0100

    avances en la creacion del board

--- new_version/modelo.h
+++ new_version/modelo.h
@@ -15,8 +15,40 @@ int offset(int i, int j){
 }
 
 int* create_board(){
+    /*
+     *      _ _ _ 1 1 1 _ _ _
+     *      _ _ _ 1 1 1 _ _ _
+     *      _ _ _ 1 1 1 _ _ _
+     *      1 1 1 1 1 1 1 1 1
+     *      1 1 1 1 0 1 1 1 1
+     *      1 1 1 1 1 1 1 1 1
+     *      _ _ _ 1 1 1 _ _ _
+     *      _ _ _ 1 1 1 _ _ _
+     *      _ _ _ 1 1 1 _ _ _
+     *
+     */
 
     int* board = (int*) malloc(ROW_LENGTH*COL_LENGTH*sizeof(int));
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            //first row of 3x3 squares
+            board[offset(i,j)] = -1;
+            board[offset(i+3,j)] = 1;
+            board[offset(i+6,j)] = -1;
+            //second row of 3x3
+            board[offset(i,j+3)] = 1;
+            board[offset(i+3,j+3)] = 1;
+            board[offset(i+6,j+3)] = 1;
+            //third row of 3x3
+            board[offset(i,j+6)] = -1;
+            board[offset(i+3,j+6)] = 1;
+            board[offset(i+6,j+6)] = -1;
+        }
+    }
+    
+    //The center gets emptied
+    board[offset(4,4)] = 0;
+
     return board;
 }
 
